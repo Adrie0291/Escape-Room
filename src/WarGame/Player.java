b30a4aa5e8802282599eec599d2592993c9e99e0
adriane <adriane@o4.pl>
2022-07-24 00:43:53 +0200

    czesc

---
+++ src/WarGame/Player.java
@@ -0,0 +1,16 @@
+package WarGame;
+
+import java.util.List;
+
+public class Player {
+    private String name;
+    private List<Card> cards;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public void setCards(List<Card> cards) {
+        this.cards = cards;
+    }
+}
